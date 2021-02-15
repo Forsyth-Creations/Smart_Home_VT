6ab8a6d79e26b9f5e564d9c5ddff2504eaa2679e
HullabalooRobotics <33364183+HullabalooRobotics@users.noreply.github.com>
2021-02-15 12:51:55 -0500

    Cleaning

---
+++ Smart_Home/src/Configuration.h
@@ -0,0 +1,28 @@
+/**
+ * This is the configuration file for the smart home system
+ * Inspired by the Marlin Firmware setup, this page will act as a 
+ * "settings" page of sorts. When a variable is defined,
+ * that functionaliy will be acitve. You will also have the option
+ * from this menu to enable debugging for various features,
+ * which will be beyond helpful.
+ * 
+ * 
+ * 
+ * Written by Henry Forsyth, Februrary 11, 2021 2:00 AM
+ * */
+ 
+
+//Activates the bluetooth functionality
+#define HC06_ACTIVE
+
+//Activates the OLCD functionality
+#define ACTIVATE_OLCD
+
+//Activates the Ultrasonic functionality
+#define SECURITY_SYSTEM
+
+//Activates the Night Light functionality, including sensor and lighting
+#define NIGHT_LIGHT
+
+//Activates the Speaker functionality
+#define SPEAKER
---
+++ Smart_Home/src/core/bluetooth/HC06.cpp
@@ -0,0 +1,26 @@
+/**
+ * 
+ * This file was written by Henry Forsyth
+ * Februrary 15, 2021
+ * 
+ * 
+ * The objective of this file is to
+ * run the bluetooth chip for the project,
+ * most likely utilizing the Serial communication
+ * protocol to impact change on the device from the GUI
+ * 
+ * Functionality can be enabled in the "Configuration.h" file
+ * Should debugging be needed, disable all other features and simply
+ * enable the "DEBUG" variable
+ **/
+
+
+#include "Configuration.h"
+
+#ifdef HC06_ACTIVE
+
+    //init code
+
+
+
+#endif
