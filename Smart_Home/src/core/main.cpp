8311b5f8c328f8024e3f82ba38a3a9587fc83666
HullabalooRobotics <33364183+HullabalooRobotics@users.noreply.github.com>
2021-04-11 23:51:24 -0400

    satge all

--- Smart_Home/src/core/main.cpp
+++ Smart_Home/src/core/main.cpp
@@ -9,13 +9,15 @@ SmartHome home = SmartHome();
 void setup()
 {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
   Serial.begin(9600);
+  Serial.println("Hello 1");
+
   home.init();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+  Serial.println("Hello 2");
   home.run();
 }
