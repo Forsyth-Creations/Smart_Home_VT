47caf150d86c07c3e0d1784274419137b847342c
Jackman1795 <78061924+Jackman1795@users.noreply.github.com>
2021-04-13 13:16:20 -0400

    LCD display

--- Smart_Home/src/Configuration.h
+++ Smart_Home/src/Configuration.h
@@ -17,7 +17,7 @@
 //#define HC06_ACTIVE
 
 //Activates the OLCD functionality
-//#define ACTIVATE_OLCD
+#define ACTIVATE_OLCD
 
 //Activates the Ultrasonic functionality
 //#define SECURITY_SYSTEM
@@ -31,5 +31,3 @@
 //#define TEMP_SENSOR
 
 //#define HUMIDITY_SENSOR
-
-//#define ACTIVATE_OLCD
